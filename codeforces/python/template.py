1815335c5d638b5a54e968f2eef6141ab1e0e419
kevinchangjk <kevinchangjk@gmail.com>
2022-11-24 23:47:27 +0800

    codeforces: add python template

---
+++ codeforces/python/template.py
@@ -0,0 +1,19 @@
+# Template for Codeforces problems
+# By @kevinchangjk
+
+# first input: usually no. of test cases
+t = int(input())
+
+
+def solve():
+    # input reception: use input() and convert to int if necessary
+    
+    # solution
+
+    # output
+    print()
+
+
+# running loop for each test case
+for _ in range(t):
+    solve()
