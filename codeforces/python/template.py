48ef17475487fdc74f33504f97a69542ee7245e0
kevinchangjk <kevinchangjk@gmail.com>
2022-11-25 01:18:25 +0800

    codeforces: modify python template

--- codeforces/python/template.py
+++ codeforces/python/template.py
@@ -1,19 +1,43 @@
 # Template for Codeforces problems
 # By @kevinchangjk
 
+import sys
+input = sys.stdin.readline
+
+# ---- Input Functions ---- #
+
+
+# takes in a single integer
+def inp():
+    return(int(input()))
+
+
+# reads and returns a list of integers
+def inlt():
+    return(list(map(int, input().split())))
+
+
+# reads and returns a list of characters, or essentially a string
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+
+
+# returns space separated integers, instead of a list
+def invr():
+    return(map(int, input().split()))
+
+
 # first input: usually no. of test cases
-t = int(input())
+tc = inp()
 
 
+# solution
 def solve():
-    # input reception: use input() and convert to int if necessary
-    
-    # solution
-
-    # output
-    print()
+    n = inp()
+    print(n)
 
 
 # running loop for each test case
-for _ in range(t):
+for t in range(tc):
     solve()
